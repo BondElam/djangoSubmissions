266d96fa9e125c6bd841532a2758f111b4a2e46f
BondElam <bond.elam@gmail.com>
2018-10-20 11:24:42 -0400

    set hosts to *

--- django_submissions/settings.py
+++ django_submissions/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'y5&(y-^_tz)&v4@p98%ooffmka7bg*g166_+=b6xjdtvo%^6*)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
