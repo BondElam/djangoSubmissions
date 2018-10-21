76850bbd9a02596664d84f19bf2a293e5744a50c
BondElam <bond.elam@gmail.com>
2018-10-21 09:46:12 -0400

    added whitenoise

--- django_submissions/settings.py
+++ django_submissions/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_submissions.urls'
@@ -136,12 +137,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'submissions/static'),
 )
 
-# added by me
-# STATIC_ROOT = os.path.join(BASE_DIR, 'submissions')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'submissions'),   
-# )
 
 # my_project/settings.py
 LOGIN_REDIRECT_URL = 'submissions'
 LOGOUT_REDIRECT_URL = 'login'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
